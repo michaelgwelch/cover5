d411d14cf600392d866fca1ee283cd0edb830c01
Michael Welch <michaelgwelch@gmail.com>
2017-01-15 10:51:51 -0600

    Remove unsafe code and other clean up
    Several clean up steps were taken
    * Removed the usage of Unsafe in Cover5.hs
    * This required the usage of runRVar in Main.hs to be
      able to print
    * Made exports from Cover5 explicit (only choose5)
    * Made imports explicit (except on common types like List
      and Maybe

--- src/Cover5.hs
+++ src/Cover5.hs
@@ -1,18 +1,26 @@
-module Cover5(run) where
+{-# LANGUAGE CPP #-}
+module Cover5(choose5) where
 
-import Data.Random.Extras
-import Data.Random.Show.Unsafe
-import Data.Random.RVar
+import Data.Random.Extras(choiceExtract)
+import Data.Random(RVar)
 import Data.Maybe
 import Data.List
-import Control.Monad.State
-import Data.Functor
-import Control.Applicative
+import Control.Monad.State(StateT, evalStateT, get, put, lift)
 
+-- The follow two imports are not needed
+-- in ghc 8.0 which I wrote this in
+-- However travis ci is still at 7.8 and therfore
+-- needs them. Also the CPP option at top of file
+-- is just for this #if
+#if __GLASGOW_HASKELL__ <= 708
+import Data.Functor (<$>)
+import Control.Applicative (<*>)
+#endif
 
-run :: Int -> RVar [(Int,Char)]
-run numGames = sortBy comparePicks <$> 
+choose5 :: Int -> RVar [(Int,Char)]
+choose5 numGames = sortBy comparePicks <$> 
         evalStateT makePicks [1..numGames]
+
 
 -- | An ordering function for picks. It orders on the first field
 comparePicks :: (Int,Char) -> (Int,Char) -> Ordering
@@ -45,7 +53,7 @@ makePick = (,) <$> selectGame <*> lift flipCoin
 
 -- 'Flips' a coin. Returns 'a' or 'b'.
 flipCoin :: RVar Char
-flipCoin = snd <$> (fromJust $ choiceExtract "ab")
+flipCoin = snd <$> fromJust (choiceExtract "ab")
 -- alternatively, using the monad:
 -- flipCoin = do
 --              (_,coin) <- fromJust $ choiceExtract "ab"
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,16 @@
 module Main where
 
-import System.Environment
-import Cover5
+import System.Environment(getArgs)
+import Data.Random()
+import Data.RVar(sampleRVar)
+import Cover5(choose5)
 
 main :: IO ()
 main = do
          args <- getArgs
          let numGames = if null args then 13 else read $ head args
          putStrLn $ "Making picks for " ++ show numGames ++ " games."
-         print $ run numGames
+         results <- sampleRVar $ choose5 numGames
+         print results
+
+
