74bef459a85e6a7536bdb7f7c9862efffd6fed7d
Michael Welch <michaelgwelch@gmail.com>
2017-01-14 18:39:38 -0600

    hlint main src directory

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,6 @@ import Cover5
 main :: IO ()
 main = do
          args <- getArgs
-         let numGames = if null args then 13 else (read $ head args)
+         let numGames = if null args then 13 else read $ head args
          putStrLn $ "Making picks for " ++ show numGames ++ " games."
-         putStrLn $ show $ run numGames
+         print $ run numGames
