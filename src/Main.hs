966cfe6f67a9157dc8c48880dabb6ea90ee1ae16
Michael Welch <michaelgwelch@gmail.com>
2017-01-14 18:06:33 -0600

    Default to 13 games if no number specified

--- src/Main.hs
+++ src/Main.hs
@@ -7,4 +7,5 @@ main :: IO ()
 main = do
          args <- getArgs
          let numGames = if null args then 13 else (read $ head args)
+         putStrLn $ "Making picks for " ++ show numGames ++ " games."
          putStrLn $ show $ run numGames
