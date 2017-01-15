b0714dda108128c859eebe184aed4825e1cca264
Michael Welch <michaelgwelch@gmail.com>
2017-01-15 11:12:13 -0600

    Convert to usage of more general sample function

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
 import System.Environment(getArgs)
-import Data.Random()
-import Data.Random.RVar(sampleRVar)
+import Data.Random(sample)
 import Cover5(choose5)
 
 main :: IO ()
@@ -10,7 +9,7 @@ main = do
          args <- getArgs
          let numGames = if null args then 13 else read $ head args
          putStrLn $ "Making picks for " ++ show numGames ++ " games."
-         results <- sampleRVar $ choose5 numGames
+         results <- sample (choose5 numGames) 
          print results
 
 
