f1a4af929d2dbfaf4b5cc1dee80cdce838ccb761
Michael Welch <michaelgwelch@gmail.com>
2017-01-15 12:46:49 -0600

    Remove conditional compilation for 7.8

--- src/Cover5.hs
+++ src/Cover5.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 module Cover5(choose5) where
 
 import Data.Random.Extras(choiceExtract)
@@ -6,16 +5,6 @@ import Data.Random(RVar)
 import Data.Maybe
 import Data.List
 import Control.Monad.State(StateT, evalStateT, get, put, lift)
-
--- The follow two imports are not needed
--- in ghc 8.0 which I wrote this in
--- However travis ci is still at 7.8 and therfore
--- needs them. Also the CPP option at top of file
--- is just for this #if
-#if __GLASGOW_HASKELL__ <= 708
-import Data.Functor ((<$>))
-import Control.Applicative ((<*>))
-#endif
 
 choose5 :: Int -> RVar [(Int,Char)]
 choose5 numGames = sort <$> evalStateT makePicks [1..numGames]
