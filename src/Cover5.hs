5079f5abf35e4d409eb5efe5dd135efad83163d1
Michael Welch <michaelgwelch@gmail.com>
2017-01-15 13:50:53 -0600

    Shorten makePicks even more with replicateM)

--- src/Cover5.hs
+++ src/Cover5.hs
@@ -5,8 +5,12 @@ import Data.Random(RVar)
 import Data.Maybe
 import Data.List
 import Control.Monad.State(StateT, evalStateT, get, put, lift)
+import Control.Monad(replicateM)
 
-choose5 :: Int -> RVar [(Int,Char)]
+type GameId = Int
+type TeamId = Char
+
+choose5 :: Int -> RVar [(GameId,TeamId)]
 choose5 numGames = sort <$> evalStateT makePicks [1..numGames]
 
 
@@ -14,33 +18,23 @@ choose5 numGames = sort <$> evalStateT makePicks [1..numGames]
 --   which is the number of games to choose from.
 --   Returns a list of pairs. Each pair indicates
 --   a game id and a or b.
-makePicks :: StateT [Int] RVar [(Int,Char)]
-makePicks = sequence $ replicate 5 makePick
+makePicks :: StateT [GameId] RVar [(GameId,TeamId)]
+makePicks = replicateM 5 makePick
 
 -- A state transformer that selects a game and a team from a list of games.
 -- The state is modified to remove the selected game from the state.
-makePick :: StateT [Int] RVar (Int,Char)
+makePick :: StateT [GameId] RVar (GameId,TeamId)
 makePick = (,) <$> selectGame <*> lift flipCoin
--- alternatively, using the monad
--- makePick = do
---              gameId <- selectGame
---              teamId <- lift flipCoin
---              return (gameId,teamId)
-
 
 -- 'Flips' a coin. Returns 'a' or 'b'.
-flipCoin :: RVar Char
+flipCoin :: RVar TeamId
 flipCoin = snd <$> fromJust (choiceExtract "ab")
--- alternatively, using the monad:
--- flipCoin = do
---              (_,coin) <- fromJust $ choiceExtract "ab"
---              return coin
 
 -- A state transformer that selects a game identifier 
 -- from the list of identifiers
 -- stored in the state. The state is modified to remove
 -- the chosen identifier.
-selectGame :: StateT [Int] RVar Int
+selectGame :: StateT [GameId] RVar GameId
 selectGame = do
                 gs <- get
                 (gs',g) <- lift $ fromJust $ choiceExtract gs
