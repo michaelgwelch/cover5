2fee15030f381a551304bdac6dcb04f1d75f689c
Michael Welch <michaelgwelch@gmail.com>
2017-01-15 19:22:47 -0600

    move Cover5 internals into separate module

--- src/Cover5.hs
+++ src/Cover5.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Data.List
 import Control.Monad.State(StateT, evalStateT, get, put, lift)
 import Control.Monad(replicateM)
+import Cover5.Internals
 
 -- The follow two imports are not needed
 -- in ghc 8.0 which I wrote this in
@@ -16,38 +17,12 @@ import Control.Monad(replicateM)
 #if __GLASGOW_HASKELL__ <= 708
 
 import Data.Functor ((<$>))
-import Control.Applicative ((<*>))
 #endif
 
-type GameId = Int
-type TeamId = Char
 
+-- | Takes as input the number of games in a given week
+--   and returns a random list of 5 game identifiers and
+--   team identifiers
 choose5 :: Int -> RVar [(GameId,TeamId)]
 choose5 numGames = sort <$> evalStateT makePicks [1..numGames]
 
--- | Makes 5 picks for cover5. Takes an int
---   which is the number of games to choose from.
---   Returns a list of pairs. Each pair indicates
---   a game id and a or b.
-makePicks :: StateT [GameId] RVar [(GameId,TeamId)]
-makePicks = replicateM 5 makePick
-
--- A state transformer that selects a game and a team from a list of games.
--- The state is modified to remove the selected game from the state.
-makePick :: StateT [GameId] RVar (GameId,TeamId)
-makePick = (,) <$> selectGame <*> lift flipCoin
-
--- 'Flips' a coin. Returns 'a' or 'b'.
-flipCoin :: RVar TeamId
-flipCoin = snd <$> fromJust (choiceExtract "ab")
-
--- A state transformer that selects a game identifier 
--- from the list of identifiers
--- stored in the state. The state is modified to remove
--- the chosen identifier.
-selectGame :: StateT [GameId] RVar GameId
-selectGame = do
-                gs <- get
-                (gs',g) <- lift $ fromJust $ choiceExtract gs
-                put gs'
-                return g
---
+++ src/Cover5/Internals.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE CPP #-}
+module Cover5.Internals
+  (
+    TeamId(TeamA,TeamB), 
+    GameId, 
+    makePicks, 
+    makePick,
+    flipCoin,
+    selectGame
+  ) where
+
+import Data.Random.Extras(choiceExtract)
+import Data.Random(RVar)
+import Data.Maybe
+import Data.List
+import Control.Monad.State(StateT, evalStateT, get, put, lift)
+import Control.Monad(replicateM)
+
+-- The follow two imports are not needed
+-- in ghc 8.0 which I wrote this in
+-- However travis ci is still at 7.8 and therfore
+-- needs them. Also the CPP option at top of file
+-- is just for this #if
+#if __GLASGOW_HASKELL__ <= 708
+
+import Data.Functor ((<$>))
+import Control.Applicative ((<*>))
+#endif
+
+type GameId = Int
+data TeamId = TeamA | TeamB deriving (Eq, Ord, Show)
+
+-- | Makes 5 picks for cover5. Takes an int
+--   which is the number of games to choose from.
+--   Returns a list of pairs. Each pair indicates
+--   a game id and TeamA or TeamB.
+makePicks :: StateT [GameId] RVar [(GameId,TeamId)]
+makePicks = replicateM 5 makePick
+
+-- | A state transformer that randomly selects a game and a team from a list of
+--   games.  The state is modified to remove the selected game from the state.
+makePick :: StateT [GameId] RVar (GameId,TeamId)
+makePick = (,) <$> selectGame <*> lift flipCoin
+
+-- | 'Flips' a coin to pick TeamA or TeamB. 
+flipCoin :: RVar TeamId
+flipCoin = snd <$> fromJust (choiceExtract [TeamA,TeamB])
+
+-- | A state transformer that randomly selects a game identifier 
+--   from the list of identifiers stored in state. The state is modified 
+--   to remove the chosen identifier.
+selectGame :: StateT [GameId] RVar GameId
+selectGame = do
+                gs <- get
+                (gs',g) <- lift $ fromJust $ choiceExtract gs
+                put gs'
+                return g
